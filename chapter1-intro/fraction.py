b33a152998d80db8825210215fdd43e0b6f71b1d
Karl <kturrell@hotmail.com>
2016-06-04 18:33:52 +0100

    Add basic fraction class without all of the arithmetic operators and comparisons

---
+++ chapter1-intro/fraction.py
@@ -0,0 +1,41 @@
+# Fraction type
+#
+# Numerator can be any integer
+# Denominator must be an integer greater than 0
+# Negative fractions will have a negative numerator
+# Must support add/subtract/multiple/divide
+# Must display fraction in slash form with lowest terms
+
+class Fraction:
+
+   def __init__(self, top, bottom):
+      self.num = top
+      self.den = bottom
+
+   def __str__(self):
+      return str(self.num) + "/" + str(self.den)
+
+   def __repr__(self):
+      return str(self.num) + "/" + str(self.den)
+
+   def __eq__(self, other):
+      firstnum = self.num * other.den
+      secondnum = other.num * self.den
+
+      return firstnum == secondnum
+
+   def gcd(self, m, n):
+      while m % n != 0:
+         oldm = m
+         oldn = n
+         m = oldn
+         n = oldm % oldn
+      return n
+
+   def __add__(self, otherfraction):
+      newnum = (self.num * otherfraction.den) + (self.den * otherfraction.num)
+      newden = self.den * otherfraction.den
+      common = self.gcd(newnum, newden)
+
+      return Fraction(newnum // common, newden // common)
+

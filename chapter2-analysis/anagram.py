754bca7a50a6b9eda519e96fa3431732b1d6fa07
Karl <kturrell@hotmail.com>
2016-07-07 13:24:43 +0000

    Add anagram checker via sort and brute force skeleton

--- chapter2-analysis/anagram.py
+++ chapter2-analysis/anagram.py
@@ -7,6 +7,7 @@ def isAnagramCheckoff(s1, s2):
       pos2 = 0
       found = False
       while pos2 < len(otherchars) and not found:
+         print("Comparing " + s1[pos1] + " to " + otherchars[pos2])
          if s1[pos1] == otherchars[pos2]:
             found = True
             otherchars[pos2] = None
@@ -21,3 +22,26 @@ def isAnagramCheckoff(s1, s2):
       pos1 = pos1 + 1
 
    return stillOK
+
+def isAnagramSort(s1, s2):
+   alist1 = list(s1)
+   alist2 = list(s2)
+
+   alist1.sort()
+   alist2.sort()
+
+   pos = 0
+   matches = True
+
+   while pos < len(s1) and matches:
+      if alist1[pos] == alist2[pos]:
+         pos = pos + 1
+      else:
+         matches = False
+
+   return matches
+
+def isAnagramBrute(s1, s2):
+   # Generate every permutation of s1 and compare to s2
+   pass
+

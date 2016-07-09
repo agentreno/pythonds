efd9ffa0505cc1a97f4c57175849ac555f9bb3db
Karl <kturrell@hotmail.com>
2016-07-09 11:11:05 +0000

    Refactor into test functions and add list indexing analysis

--- chapter2-analysis/lists.py
+++ chapter2-analysis/lists.py
@@ -1,4 +1,7 @@
 import timeit
+import random
+
+x = None
 
 # Creating a list of consecutive integers using concatenation
 def createIntListConcat():
@@ -22,7 +25,8 @@ def createIntListComprehension():
 def createIntListRange():
    return list(range(1000)) 
 
-if __name__ == "__main__":
+def testListCreation():
+   print("==Analysis of list creation==")
    t1 = timeit.Timer("createIntListConcat()", "from __main__ import createIntListConcat")
    print("concat ", t1.timeit(number=1000), "milliseconds")
    t2 = timeit.Timer("createIntListAppend()", "from __main__ import createIntListAppend")
@@ -32,12 +36,30 @@ if __name__ == "__main__":
    t4 = timeit.Timer("createIntListRange()", "from __main__ import createIntListRange")
    print("range ", t4.timeit(number=1000), "milliseconds")
 
+def testPopFrontBack():
+   print("==Analysis of pop front vs pop end of list==")
    popzero = timeit.Timer("x.pop(0)", "from __main__ import x")
    popend = timeit.Timer("x.pop()", "from __main__ import x")
    print("pop(0)   pop()")
    for i in range(1000000,100000001,1000000):
+      global x
       x = list(range(i))
       pt = popend.timeit(number=1000)
       x = list(range(i))
       pz = popzero.timeit(number=1000)
       print("%15.5f, %15.5f" % (pz, pt))
+
+def testListIndex():
+   print("==Analysis of list indexing==")
+   for i in range(1000000,100000001,1000000):
+      global x
+      x = list(range(i))
+      lindex = timeit.Timer("x[random.randrange(%d)]" % i,
+            "from __main__ import random, x")
+      indexTime = lindex.timeit(number=1000)
+      print("%d,%10.3f" % (i, indexTime))
+
+if __name__ == "__main__":
+   testListCreation()
+   testPopFrontBack()
+   testListIndex()

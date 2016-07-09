b817f86eb733a4e9a4d6ec3bd9115dab7622a1d6
Karl <kturrell@hotmail.com>
2016-07-09 10:30:14 +0000

    Add analysis of pop timings from front and end of lists

--- chapter2-analysis/lists.py
+++ chapter2-analysis/lists.py
@@ -32,3 +32,12 @@ if __name__ == "__main__":
    t4 = timeit.Timer("createIntListRange()", "from __main__ import createIntListRange")
    print("range ", t4.timeit(number=1000), "milliseconds")
 
+   popzero = timeit.Timer("x.pop(0)", "from __main__ import x")
+   popend = timeit.Timer("x.pop()", "from __main__ import x")
+   print("pop(0)   pop()")
+   for i in range(1000000,100000001,1000000):
+      x = list(range(i))
+      pt = popend.timeit(number=1000)
+      x = list(range(i))
+      pz = popzero.timeit(number=1000)
+      print("%15.5f, %15.5f" % (pz, pt))

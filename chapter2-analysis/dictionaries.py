f1367832cbaab78fb3ebdc681abce01ff32c2b8e
Karl <kturrell@hotmail.com>
2016-07-09 16:00:17 +0000

    Reduce testing range for dict access analysis

--- chapter2-analysis/dictionaries.py
+++ chapter2-analysis/dictionaries.py
@@ -1,7 +1,7 @@
 import timeit
 import random
 
-for i in range(10000,1000001,20000):
+for i in range(10000,100001,10000):
    t = timeit.Timer("random.randrange(%d) in x" % i,
          "from __main__ import random, x")
    x = list(range(i))

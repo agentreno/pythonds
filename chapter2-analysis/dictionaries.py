493e3289a75cb2bee3eb4e2b5ba62c5e76beb353
Karl <kturrell@hotmail.com>
2016-07-10 13:40:55 +0000

    Add dictionary get item analysis

--- chapter2-analysis/dictionaries.py
+++ chapter2-analysis/dictionaries.py
@@ -1,6 +1,7 @@
 import timeit
 import random
 
+print("List vs Dict contains")
 for i in range(10000,100001,10000):
    t = timeit.Timer("random.randrange(%d) in x" % i,
          "from __main__ import random, x")
@@ -10,3 +11,21 @@ for i in range(10000,100001,10000):
    x = { j:None for j in range(i) }
    d_time = t.timeit(number=1000)
    print("%d,%10.3f,%10.3f" % (i, lst_time, d_time))
+
+print("Dict set value")
+for i in range(10000,100001,10000):
+   t = timeit.Timer("x[0] = 1",
+         "from __main__ import x")
+   x = { k:None for k in range(i) }
+   set_time = t.timeit(number=1000)
+   
+   print("%d,%10.8f" % (i, set_time))
+
+print("Dict get item")
+for i in range(10000,100001,10000):
+   t = timeit.Timer("x.get(0)",
+         "from __main__ import x")
+   x = {k:None for k in range(i)}
+   get_time = t.timeit(number=1000)
+
+   print("%d,%10.8f" % (i, get_time))

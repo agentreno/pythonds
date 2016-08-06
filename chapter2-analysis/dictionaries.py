10ab365ac9955368ded1e563895ecc7f0beab8c9
Karl <kturrell@hotmail.com>
2016-08-06 14:32:44 +0000

    Add delete tests for lists vs dictionaries

--- chapter2-analysis/dictionaries.py
+++ chapter2-analysis/dictionaries.py
@@ -29,3 +29,31 @@ for i in range(10000,100001,10000):
    get_time = t.timeit(number=1000)
 
    print("%d,%10.8f" % (i, get_time))
+
+print("List vs Dict delete first element")
+for i in range(10000,100001,10000):
+   t = timeit.Timer("del x[0]",
+         "from __main__ import x")
+   x = list(range(i))
+   lst_time = t.timeit(number=1000)
+   
+   t = timeit.Timer("del x[0]",
+         "from __main__ import x")
+   d_time = 0
+   for k in range(1000):
+      x = { j:None for j in range(i) }
+      d_time += t.timeit(number=1)
+   d_time = d_time / 1000
+   print("%d,%10.8f,%10.8f" % (i, lst_time, d_time))
+
+print("List vs Dict delete last element")
+for i in range(10000,100001,10000):
+   t = timeit.Timer("del x[len(x) - 1]",
+         "from __main__ import x")
+   x = list(range(i))
+   lst_time = t.timeit(number=1000)
+   
+   x = { j:None for j in range(i) }
+   d_time = t.timeit(number=1000)
+   print("%d,%10.8f,%10.8f" % (i, lst_time, d_time))
+

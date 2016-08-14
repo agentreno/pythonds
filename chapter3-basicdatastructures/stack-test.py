1faedfc9a7535dd27dd4f6b6da0161f3852561a9
Karl <kturrell@hotmail.com>
2016-08-14 08:18:55 +0000

    Add basic stack implementation for chapter 3

---
+++ chapter3-basicdatastructures/stack-test.py
@@ -0,0 +1,31 @@
+import unittest
+from stack import Stack
+
+class StackTest(unittest.TestCase):
+   def setUp(self):
+      self.mystack = Stack()
+
+   def tearDown(self):
+      del(self.mystack)
+
+   def test_create_empty_stack(self):
+      self.assertEqual(self.mystack.isEmpty(), True)
+
+   def test_push_element_and_peek_stack(self):
+      self.mystack.push(1)
+      self.assertEqual(self.mystack.peek(), 1)
+
+   def test_push_and_pop_element_to_stack(self):
+      self.mystack.push(1)
+      self.assertEqual(self.mystack.isEmpty(), False)
+      self.assertEqual(self.mystack.pop(), 1)
+      self.assertEqual(self.mystack.isEmpty(), True)
+
+   def test_size_of_stack(self):
+      self.mystack.push(1)
+      self.mystack.push(2)
+      self.mystack.push(3)
+      self.assertEqual(self.mystack.size(), 3)
+
+if __name__ == '__main__':
+   unittest.main()

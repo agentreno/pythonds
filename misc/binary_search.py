b11e984c2616673d70b6727eee4f4f83a555c88c
Karl Hopkinson-Turrell <karl.hopkinsonturrell@pearson.com>
2019-05-19 14:00:43 +0100

    Improve binary search example

--- misc/binary_search.py
+++ misc/binary_search.py
@@ -22,13 +22,23 @@ def simple_search(arr, item):
         
 simple_timer = timeit.Timer("simple_search(myarray, random.randint(0, len(myarray)))", "from __main__ import myarray, simple_search, random")
 binary_timer = timeit.Timer("binary_search(myarray, random.randint(0, len(myarray)))", "from __main__ import myarray, binary_search, random")
-for i in range(10000, 100001, 10000):
+
+# Input array for 3 powers of 10 (1000, 10000, 100000) to illustrate running
+# time progression
+
+# Simple search, get average running time over 1000 runs for each input
+print("Simple search:")
+for i in [10**x for x in range(3,6)]:
     global myarray
     myarray = list(range(i))
+    print("n = {}".format(i))
     print(simple_timer.timeit(number=1000))
     
-for i in range(10000, 100001, 10000):
+# Binary search
+print("Binary search:")
+for i in [10**x for x in range(3,6)]:
     global myarray
     myarray = list(range(i))
+    print("n = {}".format(i))
     print(binary_timer.timeit(number=1000))
     
